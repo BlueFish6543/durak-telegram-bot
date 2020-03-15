54abc2c8064f1bd176b8c6a7204fd7dd936167c3
Zhe Xuan Chua <bluefish6543@gmail.com>
2020-03-15 16:49:14 +0000

    More working code

--- durak.py
+++ durak.py
@@ -18,6 +18,10 @@ bot = Bot(TOKEN)
 users = []
 chat_ids = []
 cards = {}
+deck = []
+discard_pile = []
+total_number_of_cards = 0
+trump_suit = None
 
 def update_players():
     for i in range(len(users) - 1):
@@ -42,7 +46,27 @@ def start(update, context):
     update.message.reply_text(message)
     update_players()
 
+def update_cards_left():
+    for i in range(len(users)):
+        message = 'There are {} cards left in the deck.'.format(len(deck))
+        bot.send_message(chat_id=chat_ids[i], text=message)
+
+def display_cards_info(text):
+    global trump_suit
+    for i in range(len(users)):
+        message = text
+        message += 'Your cards: '
+        for j in range(len(cards[users[i]])):
+            message += cards[users[i]][j]
+            message += ' '
+        message += '\nThe trump (last card) is {}.'.format(deck[0])
+        bot.send_message(chat_id=chat_ids[i], text=message)
+    trump_suit = str(deck[0])[1]
+    update_cards_left()
+
 def start_game(update, context):
+    global deck
+
     message = 'A game has been started. Current players: '
     for i in range(len(users)):
         message += users[i]
@@ -50,15 +74,23 @@ def start_game(update, context):
     message = message[:-2]
 
     n_decks = int(math.ceil(len(users) / 5))
-    message += '\nUsing {} decks. '.format(n_decks)
-    deck = []
+    total_number_of_cards = 52 * n_decks
+    message += '\nUsing {} deck(s). '.format(n_decks)
     for i in range(n_decks):
         deck += list(Card)
     random.shuffle(deck)
 
+    # Deal cards
     for i in range(len(users)):
-        cards[users[i]] = [deck.pop() for _ in range(7)]
+        cards[users[i]] = [str(deck.pop()) for _ in range(7)]
     
+    display_cards_info(message)
+    play_game()
+
+def play_game():
+    n_players = len(users)
+    while(len(discard_pile) < total_number_of_cards):
+        pass
 
 def main():
     updater = Updater(TOKEN, use_context=True)
